18e0fba984e3fb9af38cfec7129e9a68c6fd72bd
oms24-supekar <omsupekar24@gmail.com>
2026-01-29 21:08:01 +0530

    Add patient routes for CRUD operations

---
+++ src/routes/patients.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getPatients,
+  addPatient,
+  deletePatient
+} = require("../controllers/patientController");
+
+router.get("/", getPatients);
+router.post("/", addPatient);
+router.delete("/:id", deletePatient);
+
+module.exports = router;
